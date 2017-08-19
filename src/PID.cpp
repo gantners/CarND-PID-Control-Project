dab4b7612170be63df5a0d1b816f553939d820aa
Stefan Gantner <stefangantner@live.de>
2017-08-19 16:01:26 +0200

    first version

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,142 @@ PID::PID() {}
 
 PID::~PID() {}
 
+/*
+* Initialize PID.
+*/
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    this->p_error = 0.0;
+    this->i_error = 0.0;
+    this->d_error = 0.0;
+
+    this->best_err = 1000.0;
+
+    //count iterations passed
+    this->current_step = 0;
+
+    //stop at bridge and reset
+    this->iterations = 1000;
+
+    //according to sebastian waiting at least x current_steps to have stable values
+    this->patience_steps = 1000;
+
+    this->stage = 0;
+
+    this->pIndex = 0;
+
+    this->total_error = 0.0;
+
+    this->dp = std::vector<double>({1.0, 1.0, 1.0});
+
+    this->last_good = std::vector<double>({Kp, Kd, Ki});
 }
 
+/*
+  * Update the PID error variables given cross track error.
+  */
 void PID::UpdateError(double cte) {
+    if (current_step == 1) {
+        p_error = cte;
+    }
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += p_error;
+
+    //Parameter optimization L135
+    //if (current_step % (patience_steps + iterations) >= patience_steps) {
+        //total_error += pow(cte, 2);
+        //total_error /= iterations;
+        //cout << "total_err: " << total_error << endl;
+    //}
+
+    if (current_step % (patience_steps + iterations) == 0) {
+        if (false && total_error > 0.0)
+            twiddle();
+    }
+    current_step++;
 }
 
-double PID::TotalError() {
+/*
+  * Calculate the total PID error.
+  */
+double PID::TotalError(double Kp, double Kd, double Ki) {
+    return -p_error * Kp - d_error * Kd - i_error * Ki;
 }
 
+void PID::twiddle() {
+    double total = std::accumulate(dp.begin(), dp.end(), 0);
+    cout << "Total " << total << endl;
+
+    bool save = false;
+
+    double to_tweak = 1.0;
+    switch (pIndex) {
+        case 0:
+            to_tweak = dp[0];
+            break;
+        case 1:
+            to_tweak = dp[1];
+            break;
+        case 2:
+            to_tweak = dp[2];
+            break;
+    }
+
+    if (stage == 0) {
+        to_tweak += dp[pIndex];
+
+        if (total_error < best_err) {
+            best_err = total_error;
+            cout << "Improved1: " << total_error << endl;
+            save = true;
+            dp[pIndex] *= 1.1;
+            stage = 0; //Stay in stage 0 as long as its improving
+            cout << "-> Stage 0 improved, Param " << pIndex << endl;
+        } else {
+            to_tweak -= 2 * dp[pIndex];
+            stage = 1;
+            cout << "-> Stage 0 decreased, Param " << pIndex << endl;
+        }
+    } else if (stage == 1) {
+        if (total_error < best_err) {
+            best_err = total_error;
+            cout << "Improved2: " << best_err << endl;
+            save = true;
+            dp[pIndex] *= 1.1;
+            stage = 0; //Improving again -> level up!
+            cout << "-> Stage 1, Param " << pIndex << endl;
+        } else {
+            to_tweak += dp[pIndex];
+            dp[pIndex] *= 0.9; //Getting worse - slow down!
+            stage = 1;
+            cout << "-> Stage 1 Worst, Param " << pIndex << endl;
+        }
+
+    }
+
+    switch (pIndex) {
+        case 0:
+            Kp = to_tweak;
+            break;
+        case 1:
+            Kd = to_tweak;
+            break;
+        case 2:
+            Ki = to_tweak;
+            break;
+    }
+
+    //If we have a new best_err save the params used for it
+    if (save) {
+        this->last_good = std::vector<double>({dp[0], dp[1], dp[2]});
+    }
+
+    cout << "P: " << Kp << ", I: " << Ki << ", D: " << Kd << ", best_err: " << best_err << ", total_err: "
+         << total_error << endl;
+    total_error = 0;
+    pIndex = pIndex == 2 ? 0 : pIndex + 1;
+}
--- src/PID.h
+++ src/PID.h
@@ -1,46 +1,81 @@
 #ifndef PID_H
 #define PID_H
+#include "math.h"
+#include <random>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <iterator>
 
 class PID {
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+    /*
+    * Errors
+    */
+    double p_error;
+    double i_error;
+    double d_error;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+    /*
+    * Coefficients
+    */
+    double Kp;
+    double Ki;
+    double Kd;
 
-  /*
-  * Constructor
-  */
-  PID();
+    double best_err;
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
+    //track current iteration
+    int current_step;
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+    //twiddle max iterations
+    int iterations;
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
+    //patience
+    int patience_steps;
 
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+    //stage for twiddle
+    int stage;
+
+    //current param to tune
+    int pIndex;
+
+    double total_error;
+
+    std::vector<double> dp;
+
+    std::vector<double> last_good;
+
+    /*
+    * Constructor
+    */
+    PID();
+
+    /*
+    * Destructor.
+    */
+    virtual ~PID();
+
+    /*
+    * Initialize PID.
+    */
+    void Init(double Kp, double Ki, double Kd);
+
+    /*
+    * Update the PID error variables given cross track error.
+    */
+    void UpdateError(double cte);
+
+    /*
+    * Calculate the total PID error.
+    */
+    double TotalError(double Kp, double Ki, double Kd);
+
+    /*
+     * Tune params with twiddle algorithm
+     */
+    void twiddle();
+
 };
 
 #endif /* PID_H */
